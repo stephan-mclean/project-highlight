766b295eac5ce5ed1612dca0d245f08dd805f8a1
Stephen McLean <smclean@guidewire.com>
2019-01-21 15:42:21 +0000

    Added storybook state dependency. Updated storybook story for Input component.

--- src/components/Input/Input.stories.js
+++ src/components/Input/Input.stories.js
@@ -1,14 +1,29 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
+import { State, Store } from "@sambego/storybook-state";
 import { ThemeProvider } from "styled-components";
 import Theme from "../../theme/Theme";
 import Input from "./Input";
 
+const store = new Store({
+  inputOneVal: ""
+});
+
 const stories = storiesOf("Components", module);
-stories.add("Input", () => (
-  <ThemeProvider theme={Theme.main}>
-    <div>
-      <Input label="Input" placeholder="Placeholder" />
-    </div>
-  </ThemeProvider>
-));
+stories.add("Input", () => {
+  const onInputOneChange = e => store.set({ inputOneVal: e.target.value });
+
+  return (
+    <ThemeProvider theme={Theme.main}>
+      <State store={store}>
+        {state => [
+          <Input
+            label="Input"
+            placeholder="Placeholder"
+            input={{ value: state.inputOneVal, onChange: onInputOneChange }}
+          />
+        ]}
+      </State>
+    </ThemeProvider>
+  );
+});
