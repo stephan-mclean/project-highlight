0a6758a01fd52ac38f1c7ab61d12f3b7d383fbb9
Stephen McLean <smclean@guidewire.com>
2019-01-21 13:27:07 +0000

    Add additional items to storybook entry.

--- src/components/BookTile/BookTile.stories.js
+++ src/components/BookTile/BookTile.stories.js
@@ -9,6 +9,16 @@ const mockBookTile = {
   numEntries: 0
 };
 
+const mockBookTile2 = {
+  coverSrc: "https://via.placeholder.com/128x198",
+  numEntries: 1
+};
+
+const mockBookTile3 = {
+  coverSrc: "https://via.placeholder.com/128x198",
+  numEntries: 2
+};
+
 const stories = storiesOf("Components", module);
 stories.add("Book Tile", () => (
   <ThemeProvider theme={Theme.main}>
@@ -17,6 +27,18 @@ stories.add("Book Tile", () => (
         coverSrc={mockBookTile.coverSrc}
         numEntries={mockBookTile.numEntries}
         onClick={() => console.log("Book tile 1 on click")}
+      />
+
+      <BookTile
+        coverSrc={mockBookTile2.coverSrc}
+        numEntries={mockBookTile2.numEntries}
+        onClick={() => console.log("Book tile 2 on click")}
+      />
+
+      <BookTile
+        coverSrc={mockBookTile3.coverSrc}
+        numEntries={mockBookTile3.numEntries}
+        onClick={() => console.log("Book tile 3 on click")}
       />
     </div>
   </ThemeProvider>
