ae53ca61d2ccbeafca90651eb8078e294d42a7d2
Stephen McLean <smclean@guidewire.com>
2019-01-23 09:12:54 +0000

    Add private routes.

--- src/App.js
+++ src/App.js
@@ -1,59 +1,17 @@
 import React, { Component, Fragment } from "react";
 import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import Link from "./components/Link/Link";
 import Themes from "./theme/Theme";
-import AppContainer from "./components/AppContainer/AppContainer";
-import MainRouter from "./components/MainRouter/MainRouter";
-import {
-  TopNav,
-  TopNavHeader,
-  TopNavRightNavItem,
-  BottomNav,
-  NavItem
-} from "./components/Nav";
-import Button, {
-  OUTLINE_TYPE,
-  PRIMARY_STYLE
-} from "./components/Button/Button";
 import { ROUTES } from "./constants";
 import { PublicRoutes, PrivateRoutes } from "./components/routing";
 import "./initFaIcons";
-
-const test = (
-  <AppContainer>
-    <TopNav>
-      <TopNavHeader>
-        <Link to={`/${ROUTES.HOME}`}>TopNav</Link>
-      </TopNavHeader>
-      <Link to={`/${ROUTES.SETTINGS}`}>
-        <TopNavRightNavItem icon="cog" />
-      </Link>
-    </TopNav>
-
-    <MainRouter />
-
-    <BottomNav>
-      <Link to={`/${ROUTES.BOOKS}`}>
-        <NavItem icon="book" label="Books" />
-      </Link>
-      <Button type={OUTLINE_TYPE} buttonStyle={PRIMARY_STYLE} circle>
-        <FontAwesomeIcon icon="plus" />
-      </Button>
-      <Link to={`/${ROUTES.ENTRIES}`}>
-        <NavItem icon="sticky-note" label="Entries" />
-      </Link>
-    </BottomNav>
-  </AppContainer>
-);
 
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      authenticated: false
+      authenticated: true
     };
   }
 
--- src/components/routing/private/PrivateRoutes.js
+++ src/components/routing/private/PrivateRoutes.js
@@ -1,10 +1,59 @@
 import React from "react";
 import { Route, Redirect, Switch } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "../../Link/Link";
+import {
+  TopNav,
+  TopNavHeader,
+  TopNavRightNavItem,
+  BottomNav,
+  NavItem
+} from "../../Nav";
+import Button, { OUTLINE_TYPE, PRIMARY_STYLE } from "../../Button/Button";
+import AppContainer from "../../AppContainer/AppContainer";
+import { ROUTES } from "../../../constants";
 
 export default () => (
-  <Switch>
-    <Route exact path="/private/home" component={() => <div>HOME</div>} />
+  <AppContainer>
+    <TopNav>
+      <TopNavHeader>
+        <Link to={`/${ROUTES.HOME}`}>TopNav</Link>
+      </TopNavHeader>
+      <Link to={`/${ROUTES.SETTINGS}`}>
+        <TopNavRightNavItem icon="cog" />
+      </Link>
+    </TopNav>
 
-    <Redirect exact from="/private" to="/private/home" />
-  </Switch>
+    <Switch>
+      <Route
+        exact
+        path={`/${ROUTES.BOOKS}`}
+        component={() => <div>BOOKS</div>}
+      />
+      <Route
+        exact
+        path={`/${ROUTES.ENTRIES}`}
+        component={() => <div>ENTRIES</div>}
+      />
+      <Route
+        exact
+        path={`/${ROUTES.SETTINGS}`}
+        component={() => <div>SETTINGS</div>}
+      />
+
+      <Redirect exact from="/private" to="/private/books" />
+    </Switch>
+
+    <BottomNav>
+      <Link to={`/${ROUTES.BOOKS}`}>
+        <NavItem icon="book" label="Books" />
+      </Link>
+      <Button type={OUTLINE_TYPE} buttonStyle={PRIMARY_STYLE} circle>
+        <FontAwesomeIcon icon="plus" />
+      </Button>
+      <Link to={`/${ROUTES.ENTRIES}`}>
+        <NavItem icon="sticky-note" label="Entries" />
+      </Link>
+    </BottomNav>
+  </AppContainer>
 );
