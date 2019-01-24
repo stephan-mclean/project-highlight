91291d80419e32ec385258a70ad6a8a518d4efe4
Stephen McLean <stephenmclean@Taras-MacBook-Pro.local>
2019-01-24 15:52:58 +0000

    Add firebase init.

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,22 +1,47 @@
 import React, { Component, Fragment } from "react";
 import { connect } from "react-redux";
 import { ROUTES } from "../../constants";
+import { doLogin } from "../../actions";
+import Button, {
+  OUTLINE_TYPE,
+  PRIMARY_STYLE
+} from "../../components/Button/Button";
 
 class LoginComp extends Component {
+  constructor(props) {
+    super(props);
+
+    this.login = this.login.bind(this);
+  }
+
   componentWillMount() {
     if (this.props.currentUser) {
       this.props.history.push(`/${ROUTES.PRIVATE}`);
     }
   }
 
-  componentWillUpdate(prevProps) {
-    if (this.props.currentUser && !prevProps.currentUser) {
+  componentDidUpdate() {
+    if (this.props.currentUser) {
       this.props.history.push(`/${ROUTES.PRIVATE}`);
     }
   }
 
+  login() {
+    this.props.doLogin();
+  }
+
   render() {
-    return <Fragment>LOGIN</Fragment>;
+    return (
+      <Fragment>
+        <Button
+          type={OUTLINE_TYPE}
+          buttonStyle={PRIMARY_STYLE}
+          onClick={this.login}
+        >
+          Log in
+        </Button>
+      </Fragment>
+    );
   }
 }
 
@@ -26,4 +51,7 @@ const mapStateToProps = ({ auth }) => {
   };
 };
 
-export const Login = connect(mapStateToProps)(LoginComp);
+export const Login = connect(
+  mapStateToProps,
+  { doLogin }
+)(LoginComp);
