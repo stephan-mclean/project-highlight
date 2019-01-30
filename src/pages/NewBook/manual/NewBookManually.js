f39f555ea3495cddc3234d298fe0efa9cdc12780
Stephen McLean <smclean@guidewire.com>
2019-01-30 09:02:04 +0000

    Add validation to new book manually page.

--- src/pages/NewBook/manual/NewBookManually.js
+++ src/pages/NewBook/manual/NewBookManually.js
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { Field, reduxForm, formValueSelector } from "redux-form";
 import { uploadFile } from "../../../firebase";
+import { required } from "../../../form/validations";
 import Input from "../../../components/Input/Input";
 import FileInput from "../../../components/FileInput/FileInput";
 import TextArea from "../../../components/TextArea/TextArea";
@@ -32,14 +33,16 @@ class NewBookManually extends Component {
   }
 
   onSubmit(values) {
-    delete values["coverImgFile"];
-    values.coverSrc = this.state.coverSrc;
+    if (this.state && this.state.coverSrc) {
+      delete values["coverImgFile"];
+      values.coverSrc = this.state.coverSrc;
+    }
 
     this.props.onAddBook(values);
   }
 
   render() {
-    const { handleSubmit } = this.props;
+    const { handleSubmit, invalid, submitting, pristine } = this.props;
     return (
       <form onSubmit={handleSubmit(this.onSubmit)}>
         <Field
@@ -49,7 +52,13 @@ class NewBookManually extends Component {
           buttonType={OUTLINE_TYPE}
           component={FileInput}
         />
-        <Field name="title" label="Title" type="text" component={Input} />
+        <Field
+          validate={required}
+          name="title"
+          label="Title"
+          type="text"
+          component={Input}
+        />
         <Field name="subtitle" label="Subtitle" type="text" component={Input} />
         <Field name="description" label="Description" component={TextArea} />
 
@@ -64,6 +73,7 @@ class NewBookManually extends Component {
           type="submit"
           buttonType={OUTLINE_TYPE}
           buttonStyle={ACCENT_STYLE}
+          disabled={invalid || submitting || pristine}
         >
           Add Book
         </Button>
