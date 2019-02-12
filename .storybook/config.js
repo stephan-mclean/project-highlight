0422615ca02ebcde0a60709429bd79b04423e4bb
Stephen McLean <smclean@guidewire.com>
2019-02-12 14:52:48 +0000

    Initial commit.

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,16 @@
-import { configure } from "@storybook/react";
+import { configure, addDecorator } from "@storybook/react";
+import { withInfo } from "@storybook/addon-info";
+import { ThemeProvider } from "styled-components";
+import StoryBookContainer from "../src/components/StorybookContainer/StorybookContainer";
 import "../src/initFaIcons";
 
+addDecorator(
+  withInfo({
+    inline: true,
+    source: false,
+    propTablesExclude: [ThemeProvider, StoryBookContainer]
+  })
+);
 configure(() => {
   const req = require.context("../src/components", true, /.stories.js$/);
   req.keys().forEach(filename => req(filename));
