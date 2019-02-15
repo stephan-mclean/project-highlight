99d99f713723cfaa5bb80ac3d5f6b136334a0b7d
Stephen McLean <smclean@guidewire.com>
2019-02-15 09:36:45 +0000

    Refactored auth logic into common component to be shared by sign up and log in.

---
+++ src/pages/Authentication/Login.js
@@ -0,0 +1,63 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { ROUTES } from "../../constants";
+import { logInWithEmailAndPass } from "../../actions";
+import PublicPageContainer from "../../components/PublicPageContainer/PublicPageContainer";
+import { Heading } from "./styleHelper/styleHelper";
+import AuthHelper from "./helper/SignUpLogInHelper";
+
+class LoginComp extends Component {
+  constructor(props) {
+    super(props);
+
+    this.goToSignUp = this.goToSignUp.bind(this);
+    this.loginWithEmail = this.loginWithEmail.bind(this);
+  }
+
+  loginWithEmail(values) {
+    const { email, password } = values;
+    this.props.logInWithEmailAndPass(email, password);
+  }
+
+  goToSignUp() {
+    this.props.history.push(`/${ROUTES.SIGNUP}`);
+  }
+
+  getEmailBtnLabel() {
+    return "Log in with email";
+  }
+
+  getGoogleBtnLabel() {
+    return "Log in with google";
+  }
+
+  getFacebookBtnLabel() {
+    return "Log in with facebook";
+  }
+
+  getOrBtnLabel() {
+    return "Sign up";
+  }
+
+  render() {
+    return (
+      <PublicPageContainer>
+        <Heading>Login</Heading>
+        <AuthHelper
+          handleOrBtnRedirect={this.goToSignUp}
+          handleEmailFormSubmission={this.loginWithEmail}
+          shouldEmailFormConfirmPassword={false}
+          renderEmailBtnLabel={this.getEmailBtnLabel}
+          renderGoogleBtnLabel={this.getGoogleBtnLabel}
+          renderFacebookBtnLabel={this.getFacebookBtnLabel}
+          renderOrBtnLabel={this.getOrBtnLabel}
+        />
+      </PublicPageContainer>
+    );
+  }
+}
+
+export const Login = connect(
+  null,
+  { logInWithEmailAndPass }
+)(LoginComp);
